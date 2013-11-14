106e5a8ebd9dc3cba7984dcf54e4fe28dedc74c7
Ajeesh PU <ajeeshpu@gmail.com>
2013-11-14 22:29:54 +0530

    routes checking again

--- js/app/common/lib/Routes.js
+++ js/app/common/lib/Routes.js
@@ -5,16 +5,17 @@ define(['app/common/lib/env'],function(env){
         self.get=function(slug){
             for(var i=0;i<routes.length;i++){
                 //console.log("routes",routes,slug,routes[i].slug,routes[i].slug===slug)
+                console.log("routes",slug,routes[i].slug,slug.match(routes[i].slug+"$"))
 
                 if(routes[i].slug===slug || "/".concat(routes[i].slug)  ===slug){
                     return routes[i];
-                }else if(slug.match(/routes[i].slug$/)){
+                }else if(slug.match(routes[i].slug+"$")!==undefined){
                     return routes[i];
                 }
             }
-            if(slug.match(/env.context$/)){
+            /*if(slug.match(/env.context$/)!==undefined){
                 return self.get('/index.html');
-            }
+            }*/
             console.log("Route: "+slug+" not found")
         }
     }
