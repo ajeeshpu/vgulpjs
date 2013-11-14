54438f834fb3f3869bce44650aba5e1065e06844
Ajeesh PU <ajeeshpu@gmail.com>
2013-11-15 00:11:08 +0530

    testing again for routes..When can i mask

--- js/app/plain-html-modules/main.js
+++ js/app/plain-html-modules/main.js
@@ -1,5 +1,5 @@
 require.config({
-    baseUrl: 'js/lib',
+    baseUrl: '../js/lib',
     shim:{
         'koExternalTemplateEngine':{
             deps:['jquery','knockout'],
@@ -37,9 +37,9 @@ require.config({
 define("main",["json!app/modules/routes.json","app/common/lib/Routes",'hoganTemplateEngine',"app/plain-html-modules/vms/PlainContentVM","jquery","app/common/lib/env","require"],function(routes,Routes,hogan,PlainContentVM,$,env,require){
     'use strict';
     console.log("I have loaded this",$('head title')[0],PlainContentVM,window.location.host,env);
-    var route=new Routes(routes).get("/vgulpjs/index.html");
+    var route=new Routes(routes).get(window.location.pathname);
     var dataUrl='app'.concat("/modules/").concat(route.name).concat('/data/dealDetail.json');
-    var data=require(["json!"+dataUrl,"text!app/plain-html-modules/views"+route.module.slug],function(data,slugHtml) {
+    var data=require(["json!"+dataUrl,"text!app/plain-html-modules/views/"+route.module.slug],function(data,slugHtml) {
         data.env=env
         ko.applyBindings(new PlainContentVM(data,slugHtml),document.getElementById("sitePage"));
     })
